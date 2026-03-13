6474b065593928d789719556ea4d3719a3ae462e
Varasina Farmadani <sina@sinanonym.my.id>
2026-03-13 22:25:00 +0700

    feat(Profile): add InfoItem components to show detail info
    Signed-off-by: Varasina Farmadani <sina@sinanonym.my.id>

---
+++ composeApp/src/commonMain/kotlin/com/example/myprofileapp/ui/components/profile/InfoItem.kt
@@ -0,0 +1,34 @@
+package com.example.myprofileapp.ui.components.profile
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myprofileapp.ui.style.colorscheme.CatppuccinMocha
+
+@Composable
+fun InfoItem(icon: ImageVector, text: String, color: Color = CatppuccinMocha.text) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            tint = CatppuccinMocha.blue,
+            modifier = Modifier.size(24.dp)
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(text = text, fontSize = 16.sp, color = color)
+    }
+}
