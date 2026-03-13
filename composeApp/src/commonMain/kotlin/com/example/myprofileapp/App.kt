783deb13e3d93d9a90bd0ada51a0627c2d6fd6e5
Varasina Farmadani <sina@sinanonym.my.id>
2026-03-13 22:39:25 +0700

    feat(App): implement InfoItem into ProfileCard
    Signed-off-by: Varasina Farmadani <sina@sinanonym.my.id>

--- composeApp/src/commonMain/kotlin/com/example/myprofileapp/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/myprofileapp/App.kt
@@ -1,18 +1,20 @@
 package com.example.myprofileapp
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Badge
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material.icons.filled.Web
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -22,7 +24,7 @@ import androidx.compose.ui.unit.dp
 import com.example.myprofileapp.ui.components.profile.InfoItem
 import com.example.myprofileapp.ui.components.profile.ProfileCard
 import com.example.myprofileapp.ui.components.profile.ProfileHeader
-import org.jetbrains.compose.resources.painterResource
+import com.example.myprofileapp.ui.style.colorscheme.CatppuccinMocha
 
 import myprofileapp.composeapp.generated.resources.Res
 import myprofileapp.composeapp.generated.resources.compose_multiplatform
@@ -30,30 +32,54 @@ import myprofileapp.composeapp.generated.resources.compose_multiplatform
 @Composable
 @Preview
 fun App() {
-    var showDetailInfo by remember { mutableStateOf(true) }
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        ProfileHeader(
-            name = "Varasina Farmadani",
-            bio = "a regular human, nothing special about me."
-        )
+    MaterialTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = CatppuccinMocha.base
+        ) {
+            var showDetailInfo by remember { mutableStateOf(false) }
 
-        Spacer(modifier = Modifier.height(24.dp))
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                ProfileHeader(
+                    name = "Varasina Farmadani",
+                    bio = "a regular human, nothing special about me."
+                )
 
-        Button(onClick = {
-            showDetailInfo = !showDetailInfo
-        }) {
-            Text(if (showDetailInfo) "hide detail" else "show detail")
-        }
+                Spacer(modifier = Modifier.height(24.dp))
 
-        Spacer(modifier = Modifier.height(16.dp))
+                Button(onClick = {
+                    showDetailInfo = !showDetailInfo
+                }) {
+                    Text(if (showDetailInfo) "hide detail" else "show detail")
+                }
 
-        AnimatedVisibility(visible = showDetailInfo) {
-            ProfileCard {
+                Spacer(modifier = Modifier.height(16.dp))
+
+                AnimatedVisibility(visible = showDetailInfo) {
+                    ProfileCard {
+                        InfoItem(
+                            icon = Icons.Default.Badge,
+                            text = "123140107"
+                        )
+                        InfoItem(
+                            icon = Icons.Default.Email,
+                            text = "sina@sinanonym.my.id"
+                        )
+                        InfoItem(
+                            icon = Icons.Default.Phone,
+                            text = "+6285117788740"
+                        )
+                        InfoItem(
+                            icon = Icons.Default.Web,
+                            text = "sinanonym.my.id"
+                        )
+                    }
+                }
             }
         }
     }
