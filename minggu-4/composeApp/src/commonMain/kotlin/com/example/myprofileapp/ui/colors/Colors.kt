86eca0c6d3574d519e502de1730d95f1a2795d3f
Varasina Farmadani <sina@sinanonym.my.id>
2026-04-11 17:46:40 +0700

    feat(colors): add code for colors

--- minggu-4/composeApp/src/commonMain/kotlin/com/example/myprofileapp/ui/colors/Colors.kt
+++ minggu-4/composeApp/src/commonMain/kotlin/com/example/myprofileapp/ui/colors/Colors.kt
@@ -1,3 +1,72 @@
 package com.example.myprofileapp.ui.colors
 
-data class Colors()
+import androidx.compose.ui.graphics.Color
+import com.example.myprofileapp.ui.colors.style.colorscheme.dark.CatppuccinMocha
+import com.example.myprofileapp.ui.colors.style.colorscheme.light.CatppuccinLatte
+
+data class Colors(
+    val backgroundMain: Color,
+    val backgroundTopBar: Color,
+    val backgroundCard: Color,
+
+    val textPrimary: Color,
+    val textSecondary: Color,
+
+    val borderUnfocused: Color,
+
+    val accentPrimary: Color,
+    val accentSecondary: Color,
+    val link: Color,
+
+    val success: Color,
+    val error: Color
+)
+
+data class Theme(
+    val name: String,
+    val light: Colors,
+    val dark: Colors
+)
+
+object Themes {
+
+    val Catppuccin = Theme(
+        name = "Catppuccin",
+
+        light = Colors(
+            backgroundMain = CatppuccinLatte.base,
+            backgroundTopBar = CatppuccinLatte.crust,
+            backgroundCard = CatppuccinLatte.mantle,
+
+            textPrimary = CatppuccinLatte.text,
+            textSecondary = CatppuccinLatte.subtext0,
+
+            borderUnfocused = CatppuccinLatte.overlay0,
+
+            accentPrimary = CatppuccinLatte.mauve,
+            accentSecondary = CatppuccinLatte.blue,
+            link = CatppuccinLatte.sapphire,
+
+            success = CatppuccinLatte.green,
+            error = CatppuccinLatte.red
+        ),
+
+        dark = Colors(
+            backgroundMain = CatppuccinMocha.base,
+            backgroundTopBar = CatppuccinMocha.crust,
+            backgroundCard = CatppuccinMocha.mantle,
+
+            textPrimary = CatppuccinMocha.text,
+            textSecondary = CatppuccinMocha.subtext0,
+
+            borderUnfocused = CatppuccinMocha.overlay0,
+
+            accentPrimary = CatppuccinMocha.mauve,
+            accentSecondary = CatppuccinMocha.blue,
+            link = CatppuccinMocha.sapphire,
+
+            success = CatppuccinMocha.green,
+            error = CatppuccinMocha.red
+        )
+    )
+}
