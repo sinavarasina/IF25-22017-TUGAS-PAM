8c05df02de7dd50d94ea112bc4a3a683f0ccfa97
Varasina Farmadani <sina@sinanonym.my.id>
2026-04-12 13:15:21 +0700

    feat(navigation): add Routes.kt

---
+++ minggu-5/composeApp/src/commonMain/kotlin/com/example/myprofileapp/navigation/Routes.kt
@@ -0,0 +1,16 @@
+package com.example.myprofileapp.navigation
+
+sealed class Screen(val routes: String) {
+    object Notes : Screen("notes_list")
+    object Favorites : Screen("favorites")
+    object Profile : Screen("profile")
+    object AddNote : Screen("add_note")
+
+    object NoteDetail : Screen("note_detail/{noteId}") {
+        fun createRoute(noteId: Int) = "note_detail/$noteId"
+    }
+
+    object EditNote : Screen("edit_note/{noteId}") {
+        fun createRoute(noteId: Int) = "edit_note/$noteId"
+    }
+}
