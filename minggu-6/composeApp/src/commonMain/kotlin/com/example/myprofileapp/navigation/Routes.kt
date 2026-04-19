07a20a072f9477778d9feb70b94f982c993d3357
Varasina Farmadani <sina@sinanonym.my.id>
2026-04-19 23:39:15 +0700

    feat(routes): add NewsList & NewsDetail into routes

--- minggu-6/composeApp/src/commonMain/kotlin/com/example/myprofileapp/navigation/Routes.kt
+++ minggu-6/composeApp/src/commonMain/kotlin/com/example/myprofileapp/navigation/Routes.kt
@@ -3,6 +3,8 @@ package com.example.myprofileapp.navigation
 sealed class Screen(
     val route: String,
 ) {
+    object NewsList : Screen("news_list")
+
     object Notes : Screen("notes_list")
 
     object Favorites : Screen("favorites")
@@ -17,5 +19,9 @@ sealed class Screen(
 
     object EditNote : Screen("edit_note/{noteId}") {
         fun createRoute(noteId: Int) = "edit_note/$noteId"
+    }
+
+    object NewsDetail : Screen("news_detail/{articleId}") {
+        fun createRoute(id: Int) = "news_detail/$id"
     }
 }
