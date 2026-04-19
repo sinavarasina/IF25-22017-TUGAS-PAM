64b55297ea395e2f17e832dad1654151ea4c5709
Varasina Farmadani <sina@sinanonym.my.id>
2026-04-19 23:16:30 +0700

    feat(article): add HttpClientFactory

---
+++ minggu-6/composeApp/src/commonMain/kotlin/com/example/myprofileapp/data/remote/HttpClientFactory.kt
@@ -0,0 +1,26 @@
+package com.example.myprofileapp.data.remote
+
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+object HttpClientFactory {
+    fun create(): HttpClient =
+        HttpClient {
+            install(ContentNegotiation) {
+                json(
+                    Json {
+                        prettyPrint = true
+                        isLenient = true
+                        ignoreUnknownKeys = true
+                    },
+                )
+            }
+            install(Logging) {
+                level = LogLevel.BODY
+            }
+        }
+}
