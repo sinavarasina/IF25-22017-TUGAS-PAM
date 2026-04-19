8f42852b1dce6a5437b61321d0a39e8c2fbf10f3
Varasina Farmadani <sina@sinanonym.my.id>
2026-04-19 23:06:13 +0700

    feat(article): add Article model based on JSONPlaceholder field

---
+++ minggu-6/composeApp/src/commonMain/kotlin/com/example/myprofileapp/data/article/Article.kt
@@ -0,0 +1,11 @@
+package com.example.myprofileapp.data.article
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Article(
+    val id: Int,
+    val userId: Int,
+    val title: String,
+    val body: String,
+)
